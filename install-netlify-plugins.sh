ee8e2441ad4b9759d968a0ba94761ca35432dd1b
magz1467 <marcogeraghty@gmail.com>
2025-03-18 16:34:50 +0000

    deplo4

---
+++ install-netlify-plugins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to install Netlify plugins
+
+# Install required Netlify plugins
+npm install -D netlify-plugin-cache-killer @netlify/plugin-nextjs @netlify/plugin-functions-install-core
+
+# Output success message
+echo "Netlify plugins installed successfully!" 
