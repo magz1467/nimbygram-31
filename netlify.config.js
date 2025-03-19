abe4e64ea449a653c1c70cce1ddf992250d799b0
magz1467 <marcogeraghty@gmail.com>
2025-03-19 11:11:18 +0000

    net2

--- netlify.config.js
+++ netlify.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  plugins: [
-    {
-      // Plugin to ensure sharp is properly installed
-      package: '@netlify/plugin-functions-install-core',
-      config: {
-        includeModules: ['sharp']
-      }
-    }
-  ],
   build: {
     environment: {
       NODE_VERSION: "18",
